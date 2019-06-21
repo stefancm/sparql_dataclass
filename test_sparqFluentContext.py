d6de14753365bf9a49c85cb690fa527b514aa5b0
Ștefan Cristian Mladin <stefancristianmladin@outlook.com>
2019-06-21 22:16:10 +0300

    Sketched out some tests for fluent interface.

---
+++ test_sparqFluentContext.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestSparqlFluentContext(TestCase):
+    def test_when_having_traversed_subsequent_queries_are_performed_on_merged_rdf_graph(self):
+        pass
+
+    def test_query_when_dataclasstype_not_dataclass_assertion_error(self):
+        pass
+
+    def test_query_final(self):
+        pass
+
+    def test_query(self):
+        pass
+
+    def test_when_non_result_container_provided_then_assertion_error(self):
+        pass
--- test_sparqlLoadingContext.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class TestSparqlLoadingContext(TestCase):
-    def when_query_(self):
-        pass
